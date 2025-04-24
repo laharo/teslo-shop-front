b57af1d5fb393670d460400403d71edbc876eeca
Laura Haro Lopez <laharo@mercadona.es>
2025-04-24 19:42:13 +0200

    Primer commit - proyecto Angular teslo-shop

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      animation: {
+        fadeiN: 'fadeIn 0.3s ease-in-out'
+      },
+      keyframes: {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      }
+    },
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ['night'],
+  }
+}
